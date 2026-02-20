92174087e3ccf4d8c2c674de12b990ef7f075a2d
Snupai <nya@snupai.me>
2026-02-20 14:34:50 +0100

    refactor(auth): simplify auth export by removing internal store mutation and enhancing clarity

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,15 +1,6 @@
 import { convexAuth } from "@convex-dev/auth/server";
 import { Password } from "@convex-dev/auth/providers/Password";
-import { mutation } from "./_generated/server";
 
-export const { auth, signIn, signOut, store: internalStore, isAuthenticated } = convexAuth({
+export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [Password],
-});
-
-// Expose store as a public mutation so it can be called from the client
-export const store = mutation({
-  args: {},
-  handler: async (ctx) => {
-    return await internalStore(ctx);
-  },
 });
